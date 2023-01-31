193bb370e8d26c8a5f75ff647095ee40fd58ca4f
Pinols <parenti.stefano.mail@gmail.com>
2023-01-31 10:49:58 +0100

    Eureka, Hazelcast, Feign, ElasticSearch working :)

---
+++ src/test/java/pino/random/configserver/MicroservicestestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pino.random.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicestestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
